96de9160b0f6098db8d743e2eae26fecf8b037e2
Your Name <you@example.com>
2025-07-17 17:35:42 +0700

    Fix loi sai vendor

--- public/index.php
+++ public/index.php
@@ -10,9 +10,9 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-require __DIR__ . '/../your_project_folder/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../your_project_folder/bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 
 $app->handleRequest(Request::capture());
