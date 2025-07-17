148e86c454ccb28ed269e75ad23f1e94d048f329
Your Name <you@example.com>
2025-07-17 11:15:25 +0700

    Thêm route tạm /generate-key để tạo APP_KEY

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ Route::get('/', function () {
     return view('home');
 });
 
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/generate-key', function () {
+    Artisan::call('key:generate');
+    return '✅ APP_KEY đã được tạo thành công!';
+});
